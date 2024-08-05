92af57d950d981b4b5b6eeecd307167161e5f95d
manuel <manuelmignogna5@gmail.com>
2024-08-05 17:24:15 +0200

    updated end attempt support

--- src/routes/QuestionAnswerRouter.ts
+++ src/routes/QuestionAnswerRouter.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../utils/CustomError';
 import { QuestionAnswerController } from '../controllers/QuestionAnswerController';
+import { ensureAttemptIsNotEnded } from '../middlewares/auth';
 
 export const QuestionAnswerRouter = express.Router();
 
 QuestionAnswerRouter.post('/quizzes/:quizId/attempts/:attemptId/questions/:questionId/answers',
+    ensureAttemptIsNotEnded,
     (req: Request, res: Response, next: NextFunction) => {
         QuestionAnswerController.save(req)
             .then((QuestionAnswer) => {
